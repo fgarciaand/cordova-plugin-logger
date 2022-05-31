8c91cebd0d5d6458e4935659c958222e7f88aaaa
Fabricio Garcia <fgarcia@todo1.net>
2022-05-31 15:55:24 -0300

    updated: comments log function params

--- www/logger.js
+++ www/logger.js
@@ -5,7 +5,7 @@ const Logger = {
     /**
      * @function log (logLevel)
      * @description función para escribir mensajes en el log indicando el nivel
-     * @param {level} [String] - Nivel
+     * @param {level} [String] - Nivel ["LOG", "ERROR", "WARN", "INFO", "DEBUG"]
      * @param {message} [String] - Mensaje
      * @returns {Promise} - Devuelve una promesa con resultado.
      */
